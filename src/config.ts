66ca54667daf54422160ec8602824a0343729d91
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2025-02-12 15:37:47 -0600

    Update Hero component layout, modify site metadata, and add lite-youtube dependency

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Celebrando 15 años contigo Portal Académico del CCH';
-export const SITE_DESCRIPTION = 'Para celebrar los 15 años de la creación del Portal Académico del CCH, hemos preparado una serie de actividades que se detallan en esta página.';
-export const SITE_URL = 'https://portalacademico.cch.unam.mx/15aniversario/';
+export const SITE_TITLE = 'Generador reactor de amoníaco';
+export const SITE_DESCRIPTION = 'En está páginas encontrarás información sobre el generador reactor de amoníaco';
+export const SITE_URL = 'https://portalacademico.cch.unam.mx/generador-reactor-amoniaco/';
 export const SITE_IMAGE = 'social_img.jpg';
