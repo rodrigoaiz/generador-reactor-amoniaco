7fbb3d49677e5ea385a8422f161c94f9aa42b095
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2025-02-13 22:24:50 -0600

    Add Publicados component, enhance Papit styles, update InfoDos headings, and modify global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
   integrations: [icon(
     {
     include: {
-      bi: ['facebook', 'threads', 'twitter-x', 'youtube', 'instagram', 'linkedin', 'box-arrow-in-right','envelope-paper-heart','file-earmark-arrow-down', 'box-arrow-in-right', 'link-45deg', 'arrow-bar-right', 'arrow-bar-left', 'calendar', 'clock', 'pin-map', 'file-earmark-pdf-fill']
+      bi: ['facebook', 'threads', 'twitter-x', 'youtube', 'instagram', 'linkedin', 'box-arrow-in-right','envelope-paper-heart','file-earmark-arrow-down', 'box-arrow-in-right', 'link-45deg', 'arrow-bar-right', 'arrow-bar-left', 'calendar', 'clock', 'pin-map', 'file-earmark-pdf-fill', 'file-earmark-medical-fill','file-text-fill','book-fill']
     }
   }
   )]
