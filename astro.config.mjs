bf7bf42e89c5488d635ba3d066edf1e080c9be70
rodrigoaiz <rodrigoaiz@gmail.com>
2025-02-18 13:00:23 -0600

    Remove unused document, add Partytown dependency, and update layout and links for improved performance and clarity

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   base: import.meta.env.DEV ? "." : "/generador-reactor-amoniaco/",
@@ -18,5 +20,5 @@ export default defineConfig({
       bi: ['facebook', 'threads', 'twitter-x', 'youtube', 'instagram', 'linkedin', 'box-arrow-in-right','envelope-paper-heart','file-earmark-arrow-down', 'box-arrow-in-right', 'link-45deg', 'arrow-bar-right', 'arrow-bar-left', 'calendar', 'clock', 'pin-map', 'file-earmark-pdf-fill', 'file-earmark-medical-fill','file-text-fill','book-fill']
     }
   }
-  )]
+  ), partytown()]
 });
